1d6b2f0eca97724b5c9780797b5f1f22debc117e
Christopher Lopez <chlo1259@gmail.com>
2015-10-17 17:59:38 +0200

    t1

---
+++ app/pods/recipe/model.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  prepTime: DS.attr('number'),
+  cookTime: DS.attr('number'),
+  ovenTemperature: DS.attr('number'),
+  servings: DS.attr('number')
+});
--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'weekfood',
+    podModulePrefix: 'weekfood/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
