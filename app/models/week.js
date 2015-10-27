c97d9a58a4fafce6bbc904756a0414b886ce7d0f
Christopher Lopez <chlo1259@gmail.com>
2015-10-27 14:45:48 +0000

    Removed week-planner

--- app/models/week.js
+++ app/models/week.js
@@ -5,4 +5,12 @@ export default DS.Model.extend({
   year: DS.attr('number'),
   weekNumber: DS.attr('number'),
   days: DS.hasMany('day', { async : true }),
+
+  momentWeek : Ember.computed('year', 'weekNumber', function(){
+    const weekNumber = this.get('weekNumber'),
+                year = this.get('year'),
+          dateString = `${year}-${weekNumber}`;
+
+    return moment(dateString, 'YYYY-ww');
+  })
 });
